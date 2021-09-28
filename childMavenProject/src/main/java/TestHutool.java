2e1a400333c77deb7b5534b2f37daf8d501ff2d3
peter <1834235886@qq.com>
2021-09-28 16:08:16 +0800

    更改类文件4

--- childMavenProject/src/main/java/TestHutool.java
+++ childMavenProject/src/main/java/TestHutool.java
@@ -5,7 +5,7 @@ public class TestHutool {
     public static void main(String[] args) {
         String dateStr = "2012-12-12 12:12:12";
         DateTime date = DateUtil.parse(dateStr);
-        System.out.println("hello word");
+        System.out.println("hello word!");
     }
 
 }
