cfc9cb92e0bc9ebc80cdfcbb3910edd7f53d4f4f
peter <1834235886@qq.com>
2021-09-28 15:44:12 +0800

    项目第一次创建

---
+++ childMavenProject/src/main/java/TestHutool.java
@@ -0,0 +1,11 @@
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+
+public class TestHutool {
+    public static void main(String[] args) {
+        String dateStr = "2012-12-12 12:12:12";
+        DateTime date = DateUtil.parse(dateStr);
+        System.out.println(date);
+    }
+
+}
